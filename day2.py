5da70566b4c9b40dc030f5ed10e7215ecfad4c62
Krishna Parshad Bhatt <kb9291972@gmail.com>
2026-09-23 16:49:56 +0545

    main

---
+++ day2.py
@@ -0,0 +1,60 @@
+# Day 2 out of 100 learning python.
+
+print("Day 2 of learning Python.")
+print("Python is better than other languages.")
+
+#variable= A container for a value (string, integer, float, boolean)
+#           A variable behaves as if it was the value it contains.
+
+
+#string
+first_name= "Krishna"
+hobby= "Playing Games and listening to musics."
+
+print(f"Hello {first_name}")
+print(f"I love {hobby}")
+
+
+#Integer
+
+age= 18
+playing= 5
+
+print(f"You are {age} years old")
+print(f"You have been playing games for {playing} hours.")
+
+
+#Float
+
+price= 10.99
+gpa= 3.09
+distance= 6.9
+
+print(f"The price is ${price}")
+print(f"Your gpa is: {gpa}")
+print(f"You ran {distance}km.")
+
+#Boolean
+
+is_student= False
+
+if is_student:
+	print("You are a student")
+
+else:
+	print("You are not a student.")
+
+for_sale= True
+
+
+if for_sale:
+ print("That iteam is for sale")
+else:
+ print("The iteam isnt for sale.")
+
+
+
+#In day 2, I learned about Variables (string, integer, float, boolean).
+
+
+
