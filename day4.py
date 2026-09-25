728e28ed07d92d9d3d2cffc92601341b5f3f08c3
Krishna Parshad Bhatt <kb9291972@gmail.com>
2026-09-25 11:17:53 +0545

    day3

---
+++ day4.py
@@ -0,0 +1,33 @@
+#User Input.
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+age= age +1
+
+print(f"Hello, {name}")
+print(f"You are {age} age years old.")
+
+
+noun1 = input("Enter a noun: ")
+noun2 = input("Enter another noun: ")
+adjective = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+
+print(f"Once there lived a {noun1}.")
+print(f"While walking on the road, he/she saw a {adjective} {noun2},")
+print(f"but the {noun2} was not {adjective}, it {verb}ed on {noun1}")
+
+
+length= int(input("Enter the lenght: "))
+width= int(input("Enter the width: "))
+area= length * width
+
+print(f"Your area is:  {area}")
+
+length= int(input("Enter the lenght: "))
+width= int(input("Enter the width: "))
+height= int(inout("Enter the heightr "))
+volume = length * width * height
+
+print(f"The volume is:  {volume}")
+
+# Today, I learned about user input.
